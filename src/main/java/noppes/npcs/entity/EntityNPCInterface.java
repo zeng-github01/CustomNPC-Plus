767cc4bc6490104695890ca1f5be7c1cde5f112e
zeng-github01 <95841646+zeng-github01@users.noreply.github.com>
2026-08-17 23:35:19 +0800

    Revert "Disable cache sense toggle"
    This reverts commit 93646c6450f76d83c7ce350709c39d0797cb5a5d.

--- src/main/java/noppes/npcs/entity/EntityNPCInterface.java
+++ src/main/java/noppes/npcs/entity/EntityNPCInterface.java
@@ -130,7 +130,6 @@ import noppes.npcs.api.entity.ICustomNpc;
 import noppes.npcs.api.handler.data.ILine;
 import noppes.npcs.api.item.IItemStack;
 import noppes.npcs.client.EntityUtil;
-import noppes.npcs.config.ConfigExperimental;
 import noppes.npcs.config.ConfigMain;
 import noppes.npcs.config.ConfigScript;
 import noppes.npcs.constants.EnumAnimation;
@@ -2208,7 +2207,6 @@ public abstract class EntityNPCInterface extends EntityCreature implements IEnti
     }
 
     public boolean canSee(Entity entity) {
-        if (!ConfigExperimental.cacheSense) return canEntityBeSeen(entity);
         return this.getEntitySenses().canSee(entity);
     }
 
